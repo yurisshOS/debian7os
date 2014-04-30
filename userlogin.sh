97a831316a016ed87c9cc93f9b652012974919cb
yurisshOS <azmi.great@gmail.com>
2014-04-30 16:14:18 +0700

    vps debian7os

---
+++ userlogin.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# Copyright by Yurissh OpenSource
+# ================================
+# 
+
+data=( `ps aux | grep -i dropbear | awk '{print $2}'`);
+
+echo "Checking Dropbear login";
+echo "---";
+
+for PID in "${data[@]}"
+do
+	#echo "check $PID";
+	NUM=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | wc -l`;
+	USER=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $10}'`;
+	IP=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $12}'`;
+	if [ $NUM -eq 1 ]; then
+		echo "$PID - $USER - $IP";
+	fi
+done
+echo "---";
+
+data=( `ps aux | grep "\[priv\]" | sort -k 72 | awk '{print $2}'`);
+
+echo "Checking OpenSSH login";
+
+echo "---";
+for PID in "${data[@]}"
+do
+        #echo "check $PID";
+		NUM=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | wc -l`;
+		USER=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | awk '{print $9}'`;
+		IP=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | awk '{print $11}'`;
+        if [ $NUM -eq 1 ]; then
+                echo "$PID - $USER - $IP";
+        fi
+done
+
+echo "-----------------------------------------------"
+echo "Script Modified by Yurissh OpenSource";
