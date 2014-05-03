df687b165f71d47304408259bba679b2de7942a8
Muhammad Yuris Azmi <muhammad.yuris.azmi@gmail.com>
2014-05-03 23:28:58 +0700

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -149,6 +149,7 @@ sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=443/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 109 -p 110"/g' /etc/default/dropbear
 echo "/bin/false" >> /etc/shells
 echo "/usr/sbin/nologin" >> /etc/shells
+service ssh restart
 service dropbear restart
 
 # install vnstat gui
