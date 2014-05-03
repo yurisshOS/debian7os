431a4d3e00cc176f1c615cec8972166acf8adf69
Muhammad Yuris Azmi <muhammad.yuris.azmi@gmail.com>
2014-05-03 23:30:07 +0700

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -236,8 +236,8 @@ echo ""  | tee -a log-install.txt
 echo "Service"  | tee -a log-install.txt
 echo "-------"  | tee -a log-install.txt
 echo "OpenVPN  : TCP 1194 (client config : http://$MYIP:81/client.tar)"  | tee -a log-install.txt
-echo "OpenSSH  : 22, 80"  | tee -a log-install.txt
-echo "Dropbear : 443"  | tee -a log-install.txt
+echo "OpenSSH  : 22, 80, 143"  | tee -a log-install.txt
+echo "Dropbear : 443, 110, 109"  | tee -a log-install.txt
 echo "Squid3   : 8080 (limit to IP SSH)"  | tee -a log-install.txt
 echo "badvpn   : badvpn-udpgw port 7300"  | tee -a log-install.txt
 echo "nginx    : 81"  | tee -a log-install.txt
